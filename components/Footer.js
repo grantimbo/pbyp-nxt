b46b7ea1404dd1d4ecb11a9e0d63fe4281928c38
grantimbo <grant.imbo@gmail.com>
2021-12-02 02:13:03 +0800

    Fetch dynamic content.

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,18 @@
 import SocialIcons from "./SocialIcons";
+import { useContext } from "react";
+import { UserContext } from "../support/context";
 
 const Footer = () => {
+  const ctx = useContext(UserContext);
+  const { contents } = ctx;
+
+  console.log(contents);
   return (
     <footer className="bg-[#080808]" id="footer">
       <div className="container px-4 pt-24 pb-10 mx-auto text-center">
-        <h1 className="title-type">Give Us a Shout</h1>
+        <h1 className="title-type">{contents?.footer?.[0]?.text}</h1>
         <div className="text-2xl md:text-4xl bg-white px-6 pt-3 pb-5 inline-block text-black rounded-md">
-          📧 : poweredbypen@gmail.com
+          {`📧 : ${contents?.email?.[0]?.text}`}
         </div>
 
         <h2 className="text-2xl font-light mt-20">Let's Be Friends</h2>
--- support/context.js
+++ support/context.js
@@ -1,23 +1,36 @@
-import { getProjects } from "../support/prismic";
+import { getProjects, getPage } from "../support/prismic";
 import { useState, useEffect, createContext } from "react";
 
 export const UserContext = createContext([]);
 
 const UserContextComponent = ({ children }) => {
   const [projects, setProjects] = useState([]);
+  const [contents, setContents] = useState([]);
 
   const fetchProjects = async () => {
     const res = await getProjects("projects");
     setProjects([res.results]);
-    console.log([res.results]);
+  };
+
+  const fetchContent = async () => {
+    const res = await getPage("YNX4NBIAACoATUBy");
+    setContents(Object.assign({}, [res?.results?.[0]?.data]));
   };
 
   useEffect(() => {
     fetchProjects();
+    fetchContent();
   }, []);
 
   return (
-    <UserContext.Provider value={projects}>{children}</UserContext.Provider>
+    <UserContext.Provider
+      value={{
+        projects: projects[0],
+        contents: contents[0],
+      }}
+    >
+      {children}
+    </UserContext.Provider>
   );
 };
 
